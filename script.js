ba836f6fbd8e9f7ddd5110bc6ebad2a4102c7dea
charlesofili05 <166115638+charlesofili05@users.noreply.github.com>
2024-05-11 22:41:17 +0300

    changes

--- script.js
+++ script.js
@@ -4,32 +4,34 @@ document.getElementById("burger-menu").addEventListener("click", function () {
   document.getElementById("mobileTopNav").classList.toggle("fixednav");
   document.body.classList.toggle("no-scroll");
 });
+if(document.querySelector("#contactusMainForm")){
+   document.querySelector("#contactusMainForm").submit((e)=>{
+    e.preventDefault();
 
-// document.querySelector("#contactusMainForm").submit((e)=>{
-//     e.preventDefault();
+    document.querySelector('button[type="submit"]').text("Sending Message...")
 
-//     document.querySelector('button[type="submit"]').text("Sending Message...")
+    let name = document.querySelector('input[name="name"]').val()
+    let email = document.querySelector('input[name="email"]').val()
+    let number = document.querySelector('input[name="number"]').val()
+    let subject = document.querySelector('input[name="subject"]').val()
+    let message = document.querySelector('textarea[name="message"]').val()
+    let submit = document.querySelector('input[name="submit"]').val()
 
-//     let name = document.querySelector('input[name="name"]').val()
-//     let email = document.querySelector('input[name="email"]').val()
-//     let number = document.querySelector('input[name="number"]').val()
-//     let subject = document.querySelector('input[name="subject"]').val()
-//     let message = document.querySelector('textarea[name="message"]').val()
-//     let submit = document.querySelector('input[name="submit"]').val()
+    document.querySelector("#formMessage").load("mail.php",{
+        name: name,
+        email: email,
+        number: number,
+        subject: subject,
+        message: message,
+        submit: submit
+    }, function(response, status, xhr) {
+        // Revert the text of the submit button back to its original state
+        document.querySelector('button[type="submit"]').text("Send message")
+    });
 
-//     document.querySelector("#formMessage").load("mail.php",{
-//         name: name,
-//         email: email,
-//         number: number,
-//         subject: subject,
-//         message: message,
-//         submit: submit
-//     }, function(response, status, xhr) {
-//         // Revert the text of the submit button back to its original state
-//         document.querySelector('button[type="submit"]').text("Send message")
-//     });
+}) 
+}
 
-// })
 
 //check if the elmeent has entered viewport
 function isInViewport(element) {
